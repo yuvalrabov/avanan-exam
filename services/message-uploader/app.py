1572840b52c3900f5d025b86e463ad7b6bc78052
unknown <yuvalrabovsky@gmail.com>
2025-01-10 23:03:50 +0200

    testing github actions

--- services/message-uploader/app.py
+++ services/message-uploader/app.py
@@ -45,6 +45,8 @@ def process_messages():
             print(f"Deleted message {message_id} from SQS.")
         except Exception as e:
             print(f"Failed to process message {message_id}: {e}")
+        
+        # testing
 
 
 if __name__ == "__main__":
