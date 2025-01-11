6c0562909236c7124c68dc285129d83cc493addc
unknown <yuvalrabovsky@gmail.com>
2025-01-11 16:41:36 +0200

    github env > github output

--- services/message-uploader/app.py
+++ services/message-uploader/app.py
@@ -45,7 +45,7 @@ def process_messages():
             print(f"Deleted message {message_id} from SQS.")
         except Exception as e:
             print(f"Failed to process message {message_id}: {e}")
-        #testing digest one servic
+        #testing digest one servi
 
 
 if __name__ == "__main__":
